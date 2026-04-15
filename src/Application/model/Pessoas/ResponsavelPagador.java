8694e0ff9d32b6d47e3bd68437502c7d1901dc58
Davi <davivinici71@gmail.com>
2026-04-15 01:00:38 -0300

    feat: Class ResponsavelPagador

--- src/Application/model/Pessoas/ResponsavelPagador.java
+++ src/Application/model/Pessoas/ResponsavelPagador.java
@@ -1,4 +1,31 @@
 package model.Pessoas;
+import model.Pessoas.Pessoa;
+import java.time.LocalDate;
+public class ResponsavelPagador extends Pessoa {
+    private String pagador;
+    private String descricao;
+    public ResponsavelPagador(String nome, LocalDate dataNascimento, String cpf, String telefone, String profissao, String descricao){
+        super(nome, dataNascimento, cpf, telefone);
+        setPagador(profissao);
+        setDescricao(descricao);
+    }
+    public String getPagador(){
+        return pagador;
+    }
+    private void setPagador(String pagador){
+        if(pagador == null || pagador.isBlank()){
+            throw new IllegalArgumentException("Profissão inválida!");
+        }
+        this.pagador = pagador;
+    }
 
-public class ResponsavelPagador {
+    public String getDescricao(){
+        return descricao;
+    }
+    private void setDescricao(String descricao){
+        if(descricao == null || descricao.isBlank()){
+            throw new IllegalArgumentException("Descrição inválida!");
+        }
+        this.descricao = descricao;
+    }
 }
