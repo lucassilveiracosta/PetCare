a8bb37c07f26390d2072c9346eee8c946ab407a2
Davi <davivinici71@gmail.com>
2026-04-14 23:36:07 -0300

    feat: pessoa.java - criando requisições de pessoa

--- src/Application/model/Pessoa.java
+++ src/Application/model/Pessoa.java
@@ -1,4 +1,19 @@
 package model;
 
-public class Pessoa {
+import java.time.LocalDateTime;
+
+public abstract class Pessoa {
+    private int id;
+    private String nome;
+    private LocalDateTime dataNascimento;
+    private String cpf;
+    private String telefone;
+    public Pessoa(String nome, LocalDateTime dataNascimento, String cpf, String Telefone){
+
+    }
+    public int getId(){
+        return id;
+    }
+
+
 }
