60abd16deb0ba794b601e2bee2579d22079407ea
lucassilveiracosta <lscosta2007@gmail.com>
2026-04-15 00:22:42 -0300

    feat: implements class hidratacao

--- src/Application/model/prontuario/Hidratacao.java
+++ src/Application/model/prontuario/Hidratacao.java
@@ -1,4 +1,4 @@
-package model;
+package model.prontuario;
 
 public class Hidratacao {
 
@@ -13,6 +13,21 @@ public class Hidratacao {
         else {
             this.desitratacao = desitratacao;
         }
+    }
 
+    public boolean isEuvolemico() {
+        return euvolemico;
+    }
+
+    public void setEuvolemico(boolean euvolemico) {
+        this.euvolemico = euvolemico;
+    }
+
+    public Double getDesitratacao() {
+        return desitratacao;
+    }
+
+    public void setDesitratacao(Double desitratacao) {
+        this.desitratacao = desitratacao;
     }
 }
