830ed2f6ca097ef9453c156503fb17e5d270b337
Davi <davivinici71@gmail.com>
2026-04-15 10:56:05 -0300

    feat: RepositorioEspecialidade

--- src/Application/model/Especialidade/RepositorioEspecialidade.java
+++ src/Application/model/Especialidade/RepositorioEspecialidade.java
@@ -3,6 +3,14 @@ package model.Especialidade;
 import java.util.ArrayList;
 
 public class RepositorioEspecialidade {
-    private ArrayList<Especialidade> especialidades = new ArrayList<>();
+    private ArrayList<Especialidade> especialidadeRep;
+
+    public ArrayList<Especialidade> getEspecialidades() {
+        return especialidadeRep;
+    }
+
+    public void setEspecialidades(ArrayList<Especialidade> especialidades){
+        especialidadeRep = especialidades;
+    }
 
 }
