ad15b8568242c991d55d8e5f9418063d60b2eba5
lucassilveiracosta <lscosta2007@gmail.com>
2026-04-17 09:01:37 -0300

    chore: colocando setters no construtor da anamnese e tirando campo da vacina

--- src/Application/model/prontuario/Anamnese.java
+++ src/Application/model/prontuario/Anamnese.java
@@ -1,19 +1,15 @@
 package model.prontuario;
 
-import repository.RepositorioVacinas;
-
 public class Anamnese {
 
     private String queixaPrinal;
     private String restricaoAlimentar;
-    private RepositorioVacinas vacinas;
     private String descricao;
 
-    public Anamnese(String queixaPrinal, String restricaoAlimentar, RepositorioVacinas vacinas, String descricao) {
-        this.queixaPrinal = queixaPrinal;
-        this.restricaoAlimentar = restricaoAlimentar;
-        this.vacinas = vacinas;
-        this.descricao = descricao;
+    public Anamnese(String queixaPrinal, String restricaoAlimentar, String descricao) {
+        setQueixaPrinal(queixaPrinal);
+        setRestricaoAlimentar(restricaoAlimentar);
+        setDescricao(descricao);
     }
 
     public String getQueixaPrinal() {
@@ -30,14 +26,6 @@ public class Anamnese {
 
     public void setRestricaoAlimentar(String restricaoAlimentar) {
         this.restricaoAlimentar = restricaoAlimentar;
-    }
-
-    public RepositorioVacinas getVacinas() {
-        return vacinas;
-    }
-
-    public void setVacinas(RepositorioVacinas vacinas) {
-        this.vacinas = vacinas;
     }
 
     public String getDescricao() {
