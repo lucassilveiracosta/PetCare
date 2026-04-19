f2545a4729caed88a40bff16bfe3df83ab85942d
Davi <davivinici71@gmail.com>
2026-04-19 16:18:41 -0300

    fix: atualizando Veterinario e resolvendo um tratamento de erro

--- src/Application/model/Pessoas/Veterinario.java
+++ src/Application/model/Pessoas/Veterinario.java
@@ -2,14 +2,18 @@ package model.Pessoas;
 import model.Pessoas.Pessoa;
 
 import java.time.LocalDate;
+import java.util.ArrayList;
 import java.util.List;
 
 public class Veterinario extends Pessoa {
     private String crmv;
     private List<String> especialidades;
+
+
     public Veterinario(String nome, LocalDate dataNascimento, String cpf, String telefone, String crmv, List<String> especialidades){
         super(nome, dataNascimento, cpf, telefone);
         setCrmv(crmv);
+        setEspecialidades(especialidades);
     }
 
     public String getCrmv(){
@@ -28,6 +32,12 @@ public class Veterinario extends Pessoa {
     }
 
     public void setEspecialidades(List<String> especialidades) {
-        this.especialidades = especialidades;
+        if(especialidades == null){
+            throw new IllegalArgumentException("A lista não pode ser nula! Se não houver especialidades, passe uma lista vazia.");
+
+        }
+        else {
+            this.especialidades = especialidades;
+        }
     }
 }
