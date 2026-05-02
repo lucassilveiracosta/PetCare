30f1e036c8194612404d756f24c39f8065a04bfc
lucassilveiracosta <lscosta2007@gmail.com>
2026-05-01 23:10:09 -0300

    feat: criação do repositorio com interface implementada

--- src/Application/repository/RepositorioNotaFiscal.java
+++ src/Application/repository/RepositorioNotaFiscal.java
@@ -1,4 +1,44 @@
 package repository;
 
-public class RepositorioNotaFiscal {
+import model.notaFiscal.NotaFiscal;
+import repository.Interface.IRepositorioNotaFiscal;
+
+import java.util.ArrayList;
+
+public class RepositorioNotaFiscal implements IRepositorioNotaFiscal {
+
+    private ArrayList<NotaFiscal> notasFiscais;
+
+
+    @Override
+    public NotaFiscal findById(int id) {
+        NotaFiscal notaFiscal = null;
+        for (NotaFiscal nf: notasFiscais) {
+            if (nf.getId() == id) {
+                notaFiscal = nf;
+            }
+        }
+        return notaFiscal;
+    }
+
+    @Override
+    public ArrayList<NotaFiscal> findAll() {
+        return notasFiscais;
+    }
+
+    @Override
+    public void update(int id) { // avaliar como implementar update, sem prints na camada de dados
+        NotaFiscal nf = findById(id);
+    }
+
+    @Override
+    public void create(NotaFiscal nf) {
+        notasFiscais.add(nf);
+    }
+
+    @Override
+    public void delete(int id) {
+        NotaFiscal nf = findById(id);
+        if (nf != null) notasFiscais.remove(nf);
+    }
 }
