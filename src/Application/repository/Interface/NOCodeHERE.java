54ac795c04d7a3afd6e4fc208451616da8c29e52
lucassilveiracosta <lscosta2007@gmail.com>
2026-05-01 21:53:49 -0300

    feat: pasta interface na camada de dados + exemples

---
+++ src/Application/repository/Interface/NOCodeHERE.java
@@ -0,0 +1,6 @@
+package repository.Interface;
+
+public interface NOCodeHERE {
+
+    public void amovico(); // Example
+}
---
+++ src/Application/repository/RepositorioNoCodeHere.java
@@ -0,0 +1,11 @@
+package repository;
+
+import repository.Interface.NOCodeHERE;
+
+public class RepositorioNoCodeHere implements NOCodeHERE {
+
+    @Override
+    public void amovico() {
+        System.out.println("S2");
+    };
+}
