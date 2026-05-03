f9cf7be4658e6d276d4d32f8bef609498ad12c9a
Davi <davivinici71@gmail.com>
2026-05-03 14:17:54 -0300

    feat: Cirurgia

--- src/Application/model/notaFiscal/Cirurgia.java
+++ src/Application/model/notaFiscal/Cirurgia.java
@@ -1,12 +1,46 @@
 package model.notaFiscal;
+import model.Pessoas.Veterinario;
 import model.animal.Animal;
 
 import java.time.LocalDateTime;
 
 public class Cirurgia extends Procedimento{
 
-    public Cirurgia(Double preco, Animal paciente, LocalDateTime dataHora, String descricao) {
+    private Veterinario veterinarioResponsavel;
+    private String tipoAnestesia;
+    private String riscoCirurgico;
+
+    public Cirurgia(Double preco, Animal paciente, LocalDateTime dataHora, String descricao, Veterinario veterinarioResponsavel, String tipoAnestesia, String riscoCirurgico) {
         super(preco, paciente, dataHora, descricao);
+        setRiscoCirurgico(riscoCirurgico);
+        setTipoAnestesia(tipoAnestesia);
+        setVeterinarioResponsavel(veterinarioResponsavel);
 
     }
+
+    public String getRiscoCirurgico() {
+        return riscoCirurgico;
+    }
+
+    public void setRiscoCirurgico(String riscoCirurgico) {
+        this.riscoCirurgico = riscoCirurgico;
+    }
+
+    public Veterinario getVeterinarioResponsavel() {
+        return veterinarioResponsavel;
+    }
+
+    public void setVeterinarioResponsavel(Veterinario veterinarioResponsavel) {
+        this.veterinarioResponsavel = veterinarioResponsavel;
+    }
+
+    public String getTipoAnestesia() {
+        return tipoAnestesia;
+    }
+
+    public void setTipoAnestesia(String tipoAnestesia) {
+        this.tipoAnestesia = tipoAnestesia;
+    }
 }
+
+
