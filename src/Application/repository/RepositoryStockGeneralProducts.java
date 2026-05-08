81ae39e1477eb431bf5606c5e112515a122781fa
Laércio Neto <laercionetomedeiros@gmail.com>
2026-05-07 21:09:51 -0300

    feat:conclui as alteraçoes, preciso que alguem revise

--- src/Application/repository/RepositoryStockGeneralProducts.java
+++ src/Application/repository/RepositoryStockGeneralProducts.java
@@ -3,19 +3,46 @@ package repository;
 import model.notaFiscal.Produto;
 import repository.Interface.IRepositoryStockGeneralProducts;
 
+import java.util.ArrayList;
+
 public class RepositoryStockGeneralProducts implements IRepositoryStockGeneralProducts {
-    private Produto produto;
-    public RepositoryStockGeneralProducts(Produto produto){
-        this.produto = produto;
+
+    private ArrayList<Produto> produtos;
+    public RepositoryStockGeneralProducts(ArrayList<Produto> produtos) {
+        this.produtos = produtos;
+    }
+
+
+    @Override
+    public Produto findById(int id) {
+        Produto produto = null;
+        for (Produto p : produtos) {
+            if (p.getId() == id) {
+                produto = p;
+            }
+        }
+        return produto;
     }
 
     @Override
-    public Produto getProduto() {
-        return null;
+    public ArrayList<Produto> findAll() {
+        return produtos;
     }
 
     @Override
-    public void setProduto(Produto produto) {
+    public void update(int id, Produto p) {
+        produtos.set(id, p);
+    }
 
+    @Override
+    public void create(Produto p) {
+        produtos.add(p);
+    }
+
+    @Override
+    public void remove(Produto p) {
+        if (p != null) {
+            produtos.remove(p);
+        }
     }
 }
--- src/Application/repository/RepositoryStockVetProducts.java
+++ src/Application/repository/RepositoryStockVetProducts.java
@@ -4,37 +4,45 @@ import model.notaFiscal.Produto;
 import repository.Interface.IRepositoryStockVetProducts;
 
 import java.util.ArrayList;
-import java.util.List;
 
 public class RepositoryStockVetProducts implements IRepositoryStockVetProducts {
 
-    private List<Produto> produtos = new ArrayList<>();
-
-    public RepositoryStockVetProducts() {
+    private ArrayList<Produto> produtos;
+    public RepositoryStockVetProducts(ArrayList<Produto> produtos) {
+        this.produtos = produtos;
     }
+
 
     @Override
     public Produto findById(int id) {
-        return produtos.stream()
-                .filter(p -> p.getId() == id)
-                .findFirst()
-                .orElse(null);
+        Produto produto = null;
+        for (Produto p : produtos) {
+            if (p.getId() == id) {
+                produto = p;
+            }
+        }
+        return produto;
     }
 
     @Override
-    public void save(Produto produto) {
-        produtos.add(produto);
+    public ArrayList<Produto> findAll() {
+        return produtos;
     }
 
     @Override
-    public List<Produto> listAll() {
-        return new ArrayList<>(produtos);
+    public void update(int id, Produto p) {
+        produtos.set(id, p);
     }
 
     @Override
-    public void delete(int id) {
-        produtos.removeIf(p -> p.getId() == id);
+    public void create(Produto p) {
+        produtos.add(p);
     }
 
-
+    @Override
+    public void remove(Produto p) {
+        if (p != null) {
+            produtos.remove(p);
+        }
+    }
 }
