a1e1726ecc0527edae8c7147bcc37f42c338743c
lucassilveiracosta <lscosta2007@gmail.com>
2026-05-14 11:20:57 -0300

    feat: new function for auth

--- src/Application/gui/Login.java
+++ src/Application/gui/Login.java
@@ -1,7 +1,14 @@
 package gui;
 
 import business.interfaces.IControllerPessoa;
+import business.model.Pessoas.Dono;
 import business.model.Pessoas.Pessoa;
+import business.model.Pessoas.ResponsavelPagador;
+import business.model.Pessoas.Veterinario;
+import business.model.Pessoas.Funcionario;
+
+import enums.TypePerson;
+import exceptions.ClassPersonNotExists;
 import exceptions.LoginConflictException;
 
 public class Login {
@@ -26,5 +33,23 @@ public class Login {
 
         return emailPessoa;
     }
-    
+
+    public TypePerson loginPersonType(Pessoa person) {
+
+        if (person instanceof Veterinario) {
+            return TypePerson.VETERINARIO;
+        }
+        else if (person instanceof Dono) {
+            return TypePerson.DONO;
+        }
+        else if (person instanceof ResponsavelPagador) {
+            return TypePerson.RESPONSAVEL_PAGADOR;
+        }
+        else if (person instanceof Funcionario) {
+            return TypePerson.FUNCIONARIO;
+        }
+        else {
+            throw new ClassPersonNotExists("This person doesn't have a subclass");
+        }
+    }
 }
