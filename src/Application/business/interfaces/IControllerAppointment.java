1ab07cde7b660fc08e87ac04e84fc01bfee2e755
Davi <124476928+Daviprogramming@users.noreply.github.com>
2026-05-14 14:52:18 -0300

    Fix: import with two ( ; )

--- src/Application/business/interfaces/IControllerAppointment.java
+++ src/Application/business/interfaces/IControllerAppointment.java
@@ -1,10 +1,10 @@
 package business.interfaces;
 import java.util.List;
-import business.model.notaFiscal.Procedimento;;
+import business.model.notaFiscal.Procedimento;
 
 public interface IControllerAppointment {
     Procedimento getById(int id);
-    void patch(int id, Procedimento dadosParciais);
+    void patch(int id, Procedimento appointment);
     List<Procedimento> getAll();
     void update(int id, Procedimento appointment);
     void delete(int id);
