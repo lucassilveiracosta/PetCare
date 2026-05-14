2b634cbd9ad18f2cc55f82782f9428ef4af002c4
lucassilveiracosta <lscosta2007@gmail.com>
2026-05-13 23:13:50 -0300

    feat: chamando metodos da interface

--- src/Application/gui/controller/AnimalController.java
+++ src/Application/gui/controller/AnimalController.java
@@ -1,14 +1,41 @@
 package gui.controller;
 
+import business.BusinessAnimal;
 import gui.controller.interfaces.IAnimalController;
 import model.animal.Animal;
 
-import java.util.ArrayList;
+import java.util.List;
 
 public class AnimalController implements IAnimalController {
-    public Animal listAnimalById(int id){}
-    public ArrayList<Animal> listAll(){}
-    public void postAnimal(Animal animal){}
-    public void updateAnimal(int id, Animal animal){}
-    public void deleteAnimal(int id){}
+    private final BusinessAnimal businessAnimal;
+
+    public AnimalController(BusinessAnimal businessAnimal) {
+        this.businessAnimal = businessAnimal;
+    }
+
+
+    @Override
+    public Animal listAnimalById(int id){
+        return businessAnimal.getById(id);
+    }
+
+    @Override
+    public List<Animal> listAll(){
+        return businessAnimal.getAll();
+    }
+
+    @Override
+    public void postAnimal(Animal animal){
+        businessAnimal.post(animal);
+    }
+
+    @Override
+    public void updateAnimal(int id, Animal animal){
+        businessAnimal.patch(id, animal);
+    }
+
+    @Override
+    public void deleteAnimal(int id){
+        businessAnimal.delete(id);
+    }
 }
--- src/Application/gui/controller/interfaces/IAnimalController.java
+++ src/Application/gui/controller/interfaces/IAnimalController.java
@@ -2,11 +2,11 @@ package gui.controller.interfaces;
 
 import model.animal.Animal;
 
-import java.util.ArrayList;
+import java.util.List;
 
 public interface IAnimalController {
     Animal listAnimalById(int id);
-    ArrayList<Animal> listAll();
+    List<Animal> listAll();
     void postAnimal(Animal animal);
     void updateAnimal(int id, Animal animal);
     void deleteAnimal(int id);
