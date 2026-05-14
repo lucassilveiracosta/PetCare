db077a1e57a7ddb3c8374b1b0c5e7c4d025a6ecf
lucassilveiracosta <lscosta2007@gmail.com>
2026-05-14 01:52:20 -0300

    feat: tentativa de implementar login com futura autenticação, import da EmailValidator no ControllerPessoa

--- src/Application/gui/Login.java
+++ src/Application/gui/Login.java
@@ -1,4 +1,30 @@
 package gui;
 
+import business.interfaces.IControllerPessoa;
+import business.model.Pessoas.Pessoa;
+import exceptions.LoginConflictException;
+
 public class Login {
+
+    private IControllerPessoa controllerPessoa;
+
+
+    public Login(IControllerPessoa controllerPessoa) {
+        this.controllerPessoa = controllerPessoa;
+    }
+
+    public Pessoa logar(String email, String password) {
+        Pessoa emailPessoa = controllerPessoa.getByEmail(email);
+
+        if (emailPessoa == null) {
+            throw new LoginConflictException("Usuário não encontrado");
+        }
+
+        if (!(emailPessoa.getPassword().equals(password))) {
+            throw new LoginConflictException("Wrong password");
+        }
+
+        return emailPessoa;
+    }
+    
 }
