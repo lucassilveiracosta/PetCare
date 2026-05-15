4c0e844501ce16f7a19775d5003cfd9b942d92dc
lucassilveiracosta <lscosta2007@gmail.com>
2026-05-14 21:39:13 -0300

    refactor: ajusting the syntax of the code to english

--- src/Application/business/controller/ControllerInvoice.java
+++ src/Application/business/controller/ControllerInvoice.java
@@ -18,38 +18,38 @@ public class ControllerInvoice implements IControllerInvoice {
     }
 
     public NotaFiscal getById(int id) {
-        if (id < 0) throw new IllegalArgumentException("ID must be positive");
-        NotaFiscal nf = repositoryInvoice.findById(id);
+        if (id < 0) throw new IllegalArgumentException("400 - ID must be positive");
+        NotaFiscal invoice = repositoryInvoice.findById(id);
 
-        if (nf == null) throw new InvoiceNotFoundException("404 - ID not found");
+        if (invoice == null) throw new InvoiceNotFoundException("404 - ID not found");
 
-        return nf;
+        return invoice;
     }
 
     public ArrayList<NotaFiscal> getAll() {
         return repositoryInvoice.findAll();
     }
 
-    public void patch(int id, NotaFiscal nf) {
-        if (id < 0) throw new IllegalArgumentException("ID must be positive");
-        if (nf == null) throw new IllegalArgumentException("Invoice can't be null");
-        NotaFiscal notaFiscal = repositoryInvoice.findById(id);
-        if (notaFiscal == null) throw new InvoiceNotFoundException("404 - ID not found");
+    public void patch(int id, NotaFiscal invoice) {
+        if (id < 0) throw new IllegalArgumentException("400 - ID must be positive");
+        if (invoice == null) throw new IllegalArgumentException("400 - Invoice can't be null");
+        NotaFiscal exists = repositoryInvoice.findById(id);
+        if (exists == null) throw new InvoiceNotFoundException("404 - ID not found");
 
-        repositoryInvoice.update(id, nf);
+        repositoryInvoice.update(id, invoice);
     }
 
     public void delete(int id) {
-        if (id < 0) throw new IllegalArgumentException("ID must be positive");
-        NotaFiscal notaFiscal = repositoryInvoice.findById(id);
-        if (notaFiscal == null) throw new InvoiceNotFoundException("404 - ID not found");
+        if (id < 0) throw new IllegalArgumentException("400 - ID must be positive");
+        NotaFiscal exists = repositoryInvoice.findById(id);
+        if (exists == null) throw new InvoiceNotFoundException("404 - ID not found");
 
-        repositoryInvoice.remove(notaFiscal);
+        repositoryInvoice.remove(exists);
     }
 
-    public void post(NotaFiscal nf) {
-        NotaFiscal exists = repositoryInvoice.findById(nf.getId());
-        if (exists != null) throw new InvoiceConflictException("This invoice already exists");
-        repositoryInvoice.create(nf);
+    public void post(NotaFiscal invoice) {
+        NotaFiscal exists = repositoryInvoice.findById(invoice.getId());
+        if (exists != null) throw new InvoiceConflictException("409 - This invoice already exists");
+        repositoryInvoice.create(invoice);
     }
 }
