18ebd9d377cf4cc667e0fc106323eb22fb48f66d
lucassilveiracosta <lscosta2007@gmail.com>
2026-05-23 18:26:51 -0300

    feat: implements crud for Hospitalization

--- src/Application/data/repository/RepositoryHospitalization.java
+++ src/Application/data/repository/RepositoryHospitalization.java
@@ -1,4 +1,54 @@
 package data.repository;
 
-public class RepositoryHospitalization {
+
+import business.model.invoice.Hospitalization;
+
+import data.interfaces.IRepositoryHospitalization;
+
+import java.util.ArrayList;
+
+public class RepositoryHospitalization implements IRepositoryHospitalization {
+    private final ArrayList<Hospitalization> hospitalizations;
+    public RepositoryHospitalization(ArrayList<Hospitalization> hospitalizations) {
+        this.hospitalizations = hospitalizations;
+    }
+
+
+    @Override
+    public Hospitalization findById(int id) {
+        Hospitalization consulta = null;
+        for (Hospitalization c : hospitalizations) {
+            if (c.getId() == id){
+                consulta = c;
+            }
+        }
+        return consulta;
+    }
+
+    @Override
+    public ArrayList<Hospitalization> findAll() {
+        return hospitalizations;
+    }
+
+    @Override
+    public void update(int id, Hospitalization c) {
+        for (int i = 0; i < hospitalizations.size(); i++) {
+            if (hospitalizations.get(i).getId() == id) {
+                hospitalizations.set(i, c);
+                return;
+            }
+        }
+    }
+
+    @Override
+    public void create(Hospitalization c) {
+        hospitalizations.add(c);
+    }
+
+    @Override
+    public void remove(Hospitalization c) {
+        if (c != null) {
+            hospitalizations.remove(c);
+        }
+    }
 }
