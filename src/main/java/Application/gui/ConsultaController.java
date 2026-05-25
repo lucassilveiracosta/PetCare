cc98bbc8d93f689c700947d547919864935c736a
vicobarreto <vlimapaesbarreto@gmail.com>
2026-05-25 15:58:21 -0300

    feat: atualização da tela consulta

---
+++ src/main/java/Application/gui/ConsultaController.java
@@ -0,0 +1,39 @@
+package gui;
+
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.*;
+import javafx.scene.layout.VBox;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class ConsultaController implements Initializable {
+
+    @FXML private ListView<String> listaEspera;
+
+    @FXML private VBox painelProntuario;
+    @FXML private Label lblNomePet;
+    @FXML private Label lblTipoAnimal;
+    @FXML private TextField campoPeso;
+    @FXML private TextArea campoSintomas;
+    @FXML private ListView<String> lvHistoricoClinico;
+    @FXML private CheckBox chkVacinaAntirrabica;
+    @FXML private Button registrarVacina;
+    @FXML private Button emitirReceitaPDF;
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        painelProntuario.setDisable(true);
+    }
+
+    @FXML
+    private void finalizarConsulta() {
+        lblNomePet.setText("Paciente: Selecione um animal");
+        lblTipoAnimal.setText("Tipo: -");
+        campoPeso.clear();
+        campoSintomas.clear();
+        chkVacinaAntirrabica.setSelected(false);
+        painelProntuario.setDisable(true);
+    }
+}
