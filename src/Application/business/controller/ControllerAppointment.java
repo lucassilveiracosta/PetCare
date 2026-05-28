62f4a0c1347bb44a85537c6fa1d58e2631df2a1a
lucassilveiracosta <lscosta2007@gmail.com>
2026-05-28 17:22:07 -0300

    feat: business layer update in appointemnt

--- src/Application/business/controller/ControllerAppointment.java
+++ src/Application/business/controller/ControllerAppointment.java
@@ -3,6 +3,7 @@ package business.controller;
 import business.interfaces.IControllerAppointment;
 import business.model.appointment.Appointment;
 import data.interfaces.IRepositoryAppointment;
+import enums.AppointmentStatus;
 import exceptions.AppointmentConflictException;
 import exceptions.AppointmentNotFoundException;
 
