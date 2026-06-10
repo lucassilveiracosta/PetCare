c433c9f3de209daee77d51af054d9c0c9c44b471
lucassilveiracosta <lscosta2007@gmail.com>
2026-06-10 20:13:12 -0300

    feat: new saveOwner function

--- src/main/java/data/SaveData.java
+++ src/main/java/data/SaveData.java
@@ -25,10 +25,41 @@ public class SaveData {
             bw.write(pet.getSex().name() + ",");
             bw.write(pet.getSize().name() + ",");
             bw.write(pet.getbirthDate().format(DateTimeFormatter.ofPattern("dd/MM/yyyy")) + ",");
-            bw.write(pet.getStageOfLife().name());
-            bw.write(pet.getOwner().getId()); // para relacionar ao dono
+            bw.write(pet.getStageOfLife().name() + ",");
+            bw.write(pet.getOwner().getId() + ""); // Salva o ID do dono para o relacionamento
             
-            bw.newLine(); // Adiciona uma nova linha para o próximo registro
+            bw.newLine();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void saveOwner(Owner owner) {
+        Locale.setDefault(Locale.US);
+        String path = "ownerSave.csv"; // Arquivo correto
+
+        try (BufferedWriter bw = new BufferedWriter(new FileWriter(path, true))) {
+            bw.write(owner.getId() + ",");
+            bw.write(owner.getName() + ",");
+            bw.write(owner.getEmail() + ",");
+            bw.write(owner.getPassword() + ",");
+            bw.write(owner.getBirthDate().format(DateTimeFormatter.ofPattern("dd/MM/yyyy")) + ",");
+            bw.write(owner.getCpf() + ",");
+            bw.write(owner.getTelephone() + ",");
+            bw.write(owner.getJob() + ",");
+            bw.write(owner.getDescription() + "");
+            
+            bw.newLine();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void saveAppointment(Appointment app) {
+        Locale.setDefault(Locale.US);
+        String path = "appointmentSave.csv";
 
         } catch (
                 IOException e) {
